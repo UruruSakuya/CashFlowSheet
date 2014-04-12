5274a62e09048e0d3c9c7123a2f0242d836a0e7c
UruruSakuya <atomic.transfer.comp@gmail.com>
2014-04-12 10:42:54 +0900

    総収入、総支出、キャッシュフロー、不労所得の計算式を追加。 所持金を追加。 ペイチェックボタン実装。

--- CashFlowSheet/public_html/js/cashflowsheet.js
+++ CashFlowSheet/public_html/js/cashflowsheet.js
@@ -19,27 +19,39 @@ function createView() {
        $(this).bind("click", removing);
     });
     
+    $("#payCheck").bind("click", function() {
+        $("#handMoney").val(parseInt($("#handMoney").val()) + parseInt($("#cashflow").val()));
+    });
+    
 }
 
 function calculate() {
     var allIncome = 0;
     var $incomeList = $("#income").find("input[type=number]");
     jQuery.each($incomeList, function() {
-        allIncome = allIncome + parseInt($(this).val());
+        if ($(this).val()) {
+            allIncome = allIncome + parseInt($(this).val());
+        }
     });
     $("#allIncome").val(allIncome);
 
     var allOutgo = 0;
     var $outgoList = $("#outgo").find("input[type=number]");
     jQuery.each($outgoList, function() {
-        allOutgo = allOutgo + parseInt($(this).val());
+        if ($(this).val()) {
+            allOutgo = allOutgo + parseInt($(this).val());
+        }
     });
     $("#allOutgo").val(allOutgo);
+    
+    $("#nonWorkedIncome").val(allIncome - parseInt($("#salary").val()));
+    
+    $("#cashflow").val(allIncome - allOutgo);
 }
 
 function init() {
     createView();
-    
+    $("#handMoney").val(0);
     $("#income").bind("change", function() {
         calculate();
     });
@@ -48,3 +60,4 @@ function init() {
         calculate();
     });
 }
+
