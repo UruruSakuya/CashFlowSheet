12acfc0265a2b0b55ca158723084c89f108acd97
UruruSakuya <atomic.transfer.comp@gmail.com>
2014-04-12 11:22:52 +0900

    所持金操作を実装。

--- CashFlowSheet/public_html/js/cashflowsheet.js
+++ CashFlowSheet/public_html/js/cashflowsheet.js
@@ -3,26 +3,54 @@ function createView() {
         $(this).parent().children(":last-child").after(
                 "<div class=\"item\"><input type=\"text\" /><input type=\"number\" step=\"10\" /></div>");
     };
-    
+
     var removing = function() {
         $items = $(this).parent().children("div");
         $items[$items.size() - 1].remove();
     };
-    
+
     $addingButtons = $("input.addingContents");
     jQuery.each($addingButtons, function() {
-       $(this).bind("click", adding);
+        $(this).bind("click", adding);
     });
-    
+
     $removingButtons = $("input.removingContents");
     jQuery.each($removingButtons, function() {
-       $(this).bind("click", removing);
+        $(this).bind("click", removing);
     });
-    
+
     $("#payCheck").bind("click", function() {
         $("#handMoney").val(parseInt($("#handMoney").val()) + parseInt($("#cashflow").val()));
     });
+
+    $("#payedHandMoney").bind("click", function() {
+        $("#pay").fadeIn(0);
+    });
+
+    $("#lossedHandMoney").bind("click", function() {
+        $("#loss").fadeIn(0);
+    });
+
+    $("#pay").find("input[type=button]").bind("click", function() {
+        if($(this).siblings("input[type=number]").val()) {
+            $("#handMoney").val(parseInt($("#handMoney").val()) + parseInt($(this).siblings("input[type=number]").val()));
+            $(this).siblings("input[type=number]").val(0);
+            $("#pay").fadeOut(0);
+        }
+    });
+
+    $("#loss").find("input[type=button]").bind("click", function() {
+        if($(this).siblings("input[type=number]").val()) {
+            $("#handMoney").val(parseInt($("#handMoney").val()) - parseInt($(this).siblings("input[type=number]").val()));
+            $(this).siblings("input[type=number]").val(0);
+            $("#loss").fadeOut(0);
+        }
+    });
     
+    $("section.modal").find("p.close").bind("click", function() {
+       $(this).parent("section").fadeOut(0);
+    });
+
 }
 
 function calculate() {
@@ -43,19 +71,19 @@ function calculate() {
         }
     });
     $("#allOutgo").val(allOutgo);
-    
+
     $("#nonWorkedIncome").val(allIncome - parseInt($("#salary").val()));
-    
+
     $("#cashflow").val(allIncome - allOutgo);
 }
 
 function init() {
     createView();
-    $("#handMoney").val(0);
+    $("input[type=number]").val(0);
     $("#income").bind("change", function() {
         calculate();
     });
-    
+
     $("#outgo").bind("change", function() {
         calculate();
     });
